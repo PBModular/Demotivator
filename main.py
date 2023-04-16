5c3d8cfd025ff658f83731edfc1fdaf1037a1184
CakesTwix <oleg.kiryazov@gmail.com>
2023-04-16 10:51:58 +0300

    demoti: photo: init

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# Module class template
-
 from base.module import BaseModule, command
 from pyrogram.types import Message
+import logging
+from pyrogram import Client
+from .utils import demot
+import os
 
-class ModuleTemplate(BaseModule):
+logger = logging.getLogger(__name__)
+
+class DemotivatorModule(BaseModule):
+    
     # Register handler
-    @command("example")
-    async def example_cmd(self, _, message: Message):
-        await message.reply(self.S["some"]["strings"])
+    @command("dmt")
+    async def dmt_cmd(self, bot: Client, message: Message):
+        # logger.info(message)
+
+        if not message.reply_to_message:
+            return await message.reply(self.S["error"]["reply"])
+        if not message.reply_to_message.photo:
+            return await message.reply(self.S["error"]["photo"])
+
+        media_path = await bot.download_media(message.reply_to_message, "demot.png")
+
+        demot.generate_demotivator(media_path, os.path.abspath(__file__).replace("main.py", "demot.png"), message.text[5:]) # Drop command
+        await message.reply_photo(os.path.abspath(__file__).replace("main.py", "demot.png"))
---
+++ utils/demot.py
@@ -0,0 +1,166 @@
+# SOURCE: https://github.com/nesclass/demotivator-bot/blob/master/bot/utils/degenerator.py
+
+import os
+import cv2
+import numpy as np
+
+from PIL import ImageFont, ImageDraw, Image
+
+FRAME_MARGIN_X = 23  # отступ до окантовки фрейма по Ox
+FRAME_MARGIN_Y = 13  # отступ до окантовки фрейма по Oy
+FRAME_THICKNESS = 1  # размер окантовки
+FRAME_INNER_PADDING = 3  # внутренний отступ до медиа (без учёта толщины)
+
+MEDIA_WIDTH = 306  # длина медиа в фрейме
+MEDIA_HEIGHT = 284  # ширина медиа в фрейме
+
+TEXT_AREA_HEIGHT = 69  # высота области для текста
+TEXT_DEFAULT_FONT_SIZE = 28  # дефолтный размер шрифта
+TEXT_CONTAINER_PADDING = 14  # отступы контейнера
+
+# margin - padding - media - padding - margin
+WIDTH = 2 * FRAME_MARGIN_X + 2 * FRAME_INNER_PADDING + MEDIA_WIDTH
+# margin - padding - media - padding - text
+HEIGHT = FRAME_MARGIN_Y + 2 * FRAME_INNER_PADDING + MEDIA_HEIGHT + TEXT_AREA_HEIGHT
+
+# margin - padding - media - padding - text
+MEDIA_TOP = FRAME_MARGIN_Y + FRAME_INNER_PADDING
+MEDIA_BOTTOM = HEIGHT - TEXT_AREA_HEIGHT - FRAME_INNER_PADDING
+
+# margin - padding - media - padding - margin
+MEDIA_LEFT = FRAME_MARGIN_X + FRAME_INNER_PADDING
+MEDIA_RIGHT = WIDTH - FRAME_MARGIN_X - FRAME_INNER_PADDING
+
+# margin - padding - container[text] - padding - margin
+MAX_TEXT_LENGTH = WIDTH - 2 * FRAME_MARGIN_X - 2 * TEXT_CONTAINER_PADDING
+
+TEMPLATE = cv2.rectangle(
+    # чёрный фон по размерам медиа
+    img=np.zeros(
+        (HEIGHT, WIDTH, 3),
+        dtype=np.uint8
+    ),
+
+    # верхний левый угол окантовки
+    pt1=(FRAME_MARGIN_X,
+         FRAME_MARGIN_Y),
+
+    # нижний правый угол окантовки
+    pt2=(WIDTH - FRAME_MARGIN_X - 1,
+         HEIGHT - TEXT_AREA_HEIGHT - 1),
+
+    color=(255, 255, 255),
+    thickness=FRAME_THICKNESS
+)
+
+# кэш для сгенерированных размеров шрифтов
+# TODO: пре-генерация? возможно целесообразнее
+cached_sizes: dict[int, ImageFont] = {}
+
+
+# достать шрифт из кэша, либо сгенерировать новый
+def get_font_by_size(size: int) -> ImageFont:
+    if size in cached_sizes:
+        return cached_sizes[size]
+
+    font = ImageFont.truetype(os.path.abspath(__file__).replace("demot.py", "../font.ttf"), size)
+    cached_sizes[size] = font
+
+    return font
+
+
+# подобрать шрифт по (размеру) текста
+def generate_font_from_text(text: str) -> ImageFont:
+    font = get_font_by_size(TEXT_DEFAULT_FONT_SIZE)
+    text_length = font.getlength(text)
+
+    # если текст больше дозволенного
+    if text_length > MAX_TEXT_LENGTH:
+        # изменение размера шрифта пропорционально превышению
+        ratio = MAX_TEXT_LENGTH / text_length
+        font_size = int(TEXT_DEFAULT_FONT_SIZE * ratio)
+        font_size += font_size % 2  # 23 -> 24, 25 -> 26
+        font = get_font_by_size(int(font_size))
+
+    return font
+
+
+# манипуляция с масштабированием и наложением фрейма
+# TODO: mp & frame-division: https://stackoverflow.com/a/55259105/20949821
+def modify_template_by_frame(template: np.ndarray, frame: np.ndarray):
+    frame = cv2.resize(
+        frame,
+        (MEDIA_WIDTH, MEDIA_HEIGHT),
+
+        # лучшее соотношение задержки к качеству
+        # чёткое изображение ценой +100-200мс оверхэда
+        interpolation=cv2.INTER_CUBIC
+    )
+
+    # замена пикселей в области y:x на фрейм
+    template[MEDIA_TOP:MEDIA_BOTTOM, MEDIA_LEFT:MEDIA_RIGHT] = frame
+
+
+# запись видео демика
+def write_video(input_file: str, output_file: str, template: np.ndarray):
+    stream = cv2.VideoCapture(input_file)
+
+    out = cv2.VideoWriter(
+        output_file,
+        cv2.VideoWriter_fourcc(*'mp4v'),
+        stream.get(cv2.CAP_PROP_FPS),
+        (WIDTH, HEIGHT)
+    )
+
+    while stream.isOpened():
+        flag, frame = stream.read()
+        if not flag:
+            break
+
+        modify_template_by_frame(template, frame)
+        out.write(template)
+
+    stream.release()
+    out.release()
+
+
+# запись фото демика
+def write_image(input_file: str, output_file: str, template: np.ndarray):
+    image = cv2.imread(input_file)
+    modify_template_by_frame(template, image)
+    cv2.imwrite(output_file, template)
+
+
+def generate_demotivator(input_file: str, output_file: str, text: str = "зачем"):
+    font = generate_font_from_text(text)
+
+    _, _, text_width, text_height = font.getbbox(text)
+    text_x = WIDTH - FRAME_MARGIN_X - TEXT_CONTAINER_PADDING - MAX_TEXT_LENGTH / 2
+    text_y = HEIGHT - TEXT_AREA_HEIGHT / 2 - 1
+
+    # opencv image (numpy matrix) -> pil image
+    image = Image.fromarray(TEMPLATE)
+
+    draw = ImageDraw.Draw(image)
+    draw.text(
+        (text_x, text_y),
+        text=text,
+        font=font,
+        fill="#fff",
+        anchor="mm"
+    )
+
+    # pil image -> opencv image (numpy matrix)
+    template = np.array(image)  # noqa
+
+    if input_file.endswith(".mp4"):  # если видео
+        return write_video(input_file, output_file, template)
+    elif input_file.endswith(".jpg"):  # если изображение
+        return write_image(input_file, output_file, template)
+    elif input_file.endswith(".png"):  # если изображение
+        return write_image(input_file, output_file, template)
+
+    # что это за херня?
+    raise NotImplementedError
+
+# generate_demotivator("/home/cakestwix/Pictures/cri.png", "deg.png")
